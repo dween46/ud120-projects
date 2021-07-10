15e43475b6c6b610da7d18c450726d984b8c3934
Deen Mohammad <dweenmohammed@gmail.com>
2021-07-11 04:35:59 +0600

    svm for eamil author classification mini project done

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -11,7 +11,7 @@
 """
     
 import sys
-#from time import time
+from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -29,8 +29,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 clf = GaussianNB()
+t0 = time()
 clf.fit(features_train, labels_train)
+print ("training time:", round(time()-t0, 3), "s")
+
+t1 = time()
 pred =  clf.predict(features_test)
+print ("predicting time:", round(time()-t1, 3), "s")
 print (accuracy_score(pred, labels_test))
 
 #########################################################
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+#features_train = features_train[:int(len(features_train)/100)]
+#labels_train = labels_train[:int(len(labels_train)/100)]
 
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+clf = svm.SVC(kernel = 'rbf', C = 10000)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print ("training time:", round(time()-t0, 3), "s")
+
+t1 = time()
+pred =  clf.predict(features_test)
+print ("predicting time:", round(time()-t1, 3), "s")
+
+print(accuracy_score(pred, labels_test))
 #########################################################
 
 
