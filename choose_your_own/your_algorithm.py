7b429df48aa17774ef7893d22764a7266269bc11
Deen Mohammad <dweenmohammed@gmail.com>
2021-07-13 02:06:19 +0600

    k nearest neighbors applied on my own

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -30,10 +30,13 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+clf = KNeighborsClassifier(n_neighbors = 4)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-
-
-
+print(accuracy_score(pred, labels_test))
 
 
 
