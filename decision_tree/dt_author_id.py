f18b153839049f8ac12120fb556441c34fc785ae
Deen Mohammad <dweenmohammed@gmail.com>
2021-07-13 00:23:09 +0600

    decision tree algorithm applied

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,7 +24,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+clf = tree.DecisionTreeClassifier(min_samples_split = 40)
+t0 = time()
+clf.fit(features_train, labels_train)
+print("time to train: ", time()- t0, "s")
+
+t1 = time()
+pred =  clf.predict(features_test)
+print("time to predict: ", time()- t1, "s")
+
+print(accuracy_score(pred, labels_test))
 
 #########################################################
 
