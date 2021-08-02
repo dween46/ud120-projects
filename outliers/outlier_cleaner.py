1667d52c93db6aef457ef3990870d9660763258d
Deen Mohammad <dweenmohammed@gmail.com>
2021-08-02 14:07:19 +0600

    outlier cleaner function completed

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,27 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
+    import math
     cleaned_data = []
 
     ### your code goes here
-
+    errors = []
+    for net, pred in zip(net_worths, predictions) :
+        errors.append(math.sqrt((net - pred)**2))
+    
+    sorted_errors = sorted(errors)
+    temp = int(math.floor(len(sorted_errors) * 0.9))
+    max_errors = sorted_errors[temp :]
+    
+    for index in range(len(net_worths)) :
+        error = (math.sqrt((net_worths[index] - predictions[index])**2))
+        
+        if error in max_errors:
+            pass
+        else:
+            cleaned_data.append([ages[index], net_worths[index], error])
     
     return cleaned_data
+
+
 
