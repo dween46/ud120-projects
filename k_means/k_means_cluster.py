3e58a752bf7d50414754c850d3da29c390233495
Deen Mohammad <dweenmohammed@gmail.com>
2021-08-09 12:39:31 +0600

    k means clustering implemented

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -39,7 +39,7 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 
 
 ### load in the dict of dicts containing all the data on each person in the dataset
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load( open("../final_project/final_project_dataset_unix.pkl", "rb") )
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
@@ -62,10 +62,19 @@ for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
+from sklearn.preprocessing import MinMaxScaler
+scl = MinMaxScaler()
+finance_features = scl.fit_transform(finance_features)
+import numpy as np
+features_test = np.array([[200000., 1000000.]])
+print (scl.transform(features_test))
+
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
-
+from sklearn.cluster import KMeans
+clf = KMeans(2)
+clf.fit(data)
+pred = clf.predict(data)
 
 
 ### rename the "name" parameter when you change the number of features
@@ -73,4 +82,8 @@ plt.show()
 try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
-    print "no predictions object named pred found, no clusters to plot"
+    print ("no predictions object named pred found, no clusters to plot")
+
+
+
+
